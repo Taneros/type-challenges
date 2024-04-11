e990dfaed060680da84af5adccff2864e99aee1b
Renat <fricentaurus@GMAIL.COM>
2024-04-11 12:10:33 +0400

    add solution to 00009

--- questions/00009-medium-deep-readonly/solution.ts
+++ questions/00009-medium-deep-readonly/solution.ts
@@ -1,3 +1,14 @@
+
+type DeepReadonly<T> = 
+ {
+  readonly [K in keyof T]: T[K] extends object
+    ? T[K] extends Function
+     ?
+      T[K]
+      : DeepReadonly<T[K]>
+        : T[K]
+}
+
 type DeepReadonly<T> = {
     readonly [K in keyof T]: T[K] extends Record<any, any> 
         ? T[K] extends Function
