29b0016ce80cdce70f14dcb731578544615bfad8
Renat <fricentaurus@GMAIL.COM>
2024-04-16 12:44:44 +0400

    add solution 00009 deep readonly

--- questions/00009-medium-deep-readonly/solution.ts
+++ questions/00009-medium-deep-readonly/solution.ts
@@ -1,18 +1,31 @@
 
-type DeepReadonly<T> = 
- {
-  readonly [K in keyof T]: T[K] extends object
+type DeepReadonly<T> =
+  {
+    readonly [K in keyof T]: T[K] extends object
     ? T[K] extends Function
-     ?
-      T[K]
-      : DeepReadonly<T[K]>
-        : T[K]
-}
+    ?
+    T[K]
+    : DeepReadonly<T[K]>
+    : T[K]
+  }
 
 type DeepReadonly<T> = {
-    readonly [K in keyof T]: T[K] extends Record<any, any> 
-        ? T[K] extends Function
-            ? T[K]
-             : DeepReadonly<T[K]>
-              : T[K]
+  readonly [K in keyof T]: T[K] extends Record<any, any>
+  ? T[K] extends Function
+  ? T[K]
+  : DeepReadonly<T[K]>
+  : T[K]
 }
+
+type DeepReadonly<T> = T extends Function
+  ? T
+  : T extends Object
+  ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+  : T
+
+
+type DeepReadonly<T> = T extends never
+  ? T
+  : keyof T extends never
+  ? T
+  : { readonly [k in keyof T]: DeepReadonly<T[k]> }
