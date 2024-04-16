6581806cb1640465a18d0a86951cd1c87d626ddf
Renat <fricentaurus@GMAIL.COM>
2024-04-16 12:16:59 +0400

    add solution 00010 tuple to union

--- questions/00010-medium-tuple-to-union/solution.ts
+++ questions/00010-medium-tuple-to-union/solution.ts
@@ -3,3 +3,8 @@ type TupleToUnion<T extends unknown[]> = T[number]
 type TupleToUnion<T> = T extends (infer R)[] 
   ? R
     : never;
+
+    type TupleToUnion<T> = T extends Array<infer Items>
+  ? Items
+    : 
+      never;
