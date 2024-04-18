a3a2d8f6b92016b8dea8af556ca39f3fa6c038ea
Renat <fricentaurus@GMAIL.COM>
2024-04-18 13:46:36 +0400

    fix solution 00012 chainable

--- questions/00012-medium-chainable-options/solution.ts
+++ questions/00012-medium-chainable-options/solution.ts
@@ -1,14 +1,8 @@
 type Chainable<T = {}> = {
-    option<
-      K extends PropertyKey,
-      V
-    >(
-      key: K extends keyof T 
-        ? V extends T[K]
-          ? never
-          : K
-        : K,
-      value: V
-      ): Chainable<{[P in keyof T as P extends K ? never : P]: T[P]} & Record<K,V>>
-    get(): T
-  }
+  option<K extends PropertyKey, V>(
+    key: K extends keyof T
+     ? never
+    : K,
+    value: V): Chainable<Omit<T, K> & Record<K , V>>
+  get(): T
+}
