ff650adaffda3be6b9a311658b58102e128cba4e
Renat <fricentaurus@GMAIL.COM>
2024-04-18 14:07:51 +0400

    add solution 00008 readonly 2

--- questions/00008-medium-readonly-2/solution.ts
+++ questions/00008-medium-readonly-2/solution.ts
@@ -6,3 +6,8 @@ type MyReadonly2<T extends {}, K extends keyof T = keyof T> =
     & {
         [P in keyof T as P extends K ? never : P]: T[P]
     }
+
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+    readonly [P in K]: T[P]
+} & Omit<T, K>
