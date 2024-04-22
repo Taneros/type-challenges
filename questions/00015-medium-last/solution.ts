8929d34ef78c9130353a6e9290ef3ecb48dc9ac3
Renat <fricentaurus@GMAIL.COM>
2024-04-22 13:47:49 +0400

    add solution 00015

--- questions/00015-medium-last/solution.ts
+++ questions/00015-medium-last/solution.ts
@@ -2,4 +2,6 @@ type Last<T extends unknown[]> = T['length'] extends [0]['length']
   ? T[0]
   : T extends [any, ...any, infer Last]
   ? Last
-  : never;
+  : never
+
+type Last<T extends any[]> = [any, ...T][T['length']]
