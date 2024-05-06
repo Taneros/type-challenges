59c13912f75486bbd3da9899eadb8fad23516506
Renat <fricentaurus@GMAIL.COM>
2024-05-06 13:02:29 +0400

    add solution 00110 Capitalize

---
+++ questions/00110-medium-capitalize/solution.ts
@@ -0,0 +1,34 @@
+interface ToUpperCase {
+    a: "A"
+    b: "B"
+    c: "C"
+    d: "D"
+    e: "E"
+    f: "F"
+    g: "G"
+    h: "H"
+    i: "I"
+    j: "J"
+    k: "K"
+    l: "L"
+    m: "M"
+    n: "N"
+    o: "O"
+    p: "P"
+    q: "Q"
+    r: "R"
+    s: "S"
+    t: "T"
+    u: "U"
+    v: "V"
+    w: "W"
+    x: "X"
+    y: "Y"
+    z: "Z"
+}
+
+type TToLowerCase = keyof ToUpperCase
+
+type MyCapitalize<S extends string> = S extends `${infer Head extends TToLowerCase}${infer Tail}` 
+  ? `${ToUpperCase[Head]}${Tail}`
+  : S
