4abe785b29169c1c064a92d6f575b224aa669806
Renat <fricentaurus@GMAIL.COM>
2024-05-08 14:40:08 +0400

    add solution 00116 replacee

--- questions/00116-medium-replace/solution.ts
+++ questions/00116-medium-replace/solution.ts
@@ -7,3 +7,10 @@ type Replace<S extends string, From extends string, To extends string> =
   : S extends `${infer Head}${infer Tail}`
     ? `${Head}${Replace<Tail, From, To>}`
     : S
+
+type Replace<S extends string, From extends string, To extends string> =
+  From extends ""
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+    ? `${L}${To}${R}`
+    : S
