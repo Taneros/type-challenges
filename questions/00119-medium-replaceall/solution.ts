27e7cfbb94d235442e91f7ba36bfd323fbcd443b
Renat <fricentaurus@GMAIL.COM>
2024-05-20 13:02:21 +0400

    add solution 00119 Replace all

--- questions/00119-medium-replaceall/solution.ts
+++ questions/00119-medium-replaceall/solution.ts
@@ -1,5 +1,11 @@
 type ReplaceAll<S extends string, From extends string, To extends string> = From extends ""
   ? S
   : S extends `${infer L}${From}${infer R}`
-    ? `${ReplaceAll<L, From, To>}${To}${ReplaceAll<R, From, To>}`
-    : S
+  ? `${ReplaceAll<L, From, To>}${To}${ReplaceAll<R, From, To>}`
+  : S
+
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ""
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+  ? `${L}${To}${ReplaceAll<R, From, To>}`
+  : S
