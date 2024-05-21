d069bb5850efba29c840191b27892ffa8a0f1397
Renat <fricentaurus@GMAIL.COM>
2024-05-21 12:57:51 +0400

    add solution 00298 Length of string

--- questions/00298-medium-length-of-string/solution.ts
+++ questions/00298-medium-length-of-string/solution.ts
@@ -1,10 +1,17 @@
 type LengthOfString<S extends string, Acc extends string[] = []> = S extends ''
   ? Acc['length']
   : S extends `${infer Head}${infer Rest}`
-    ? LengthOfString<Rest, [...Acc, Head]>
-    : never; 
+  ? LengthOfString<Rest, [...Acc, Head]>
+  : never
+
 type LengthOfStringS<S extends string, Acc extends string[] = []> = S extends ''
   ? Acc['length']
   : S extends `${string}${infer Rest}`
-    ? LengthOfStringS<Rest, [...Acc, string]>
-    : never; 
+  ? LengthOfStringS<Rest, [...Acc, string]>
+  : never
+
+type LengthOfString<S extends string, Acc extends unknown[] = []> = S['length'] extends []['length']
+  ? []['length']
+  : S extends `${infer L}${infer R}`
+  ? LengthOfString<R, [L, ...Acc]>
+  : Acc['length']
