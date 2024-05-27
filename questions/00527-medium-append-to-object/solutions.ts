c3f91e2d561bffa1f465619f2e2870f6ba0e6011
Renat <fricentaurus@GMAIL.COM>
2024-05-27 12:48:01 +0400

    add solution append to object

--- questions/00527-medium-append-to-object/solutions.ts
+++ questions/00527-medium-append-to-object/solutions.ts
@@ -2,7 +2,11 @@ type Flatten<T> = { [P in keyof T]: T[P] }
 type AppendToObject<T, U extends string, V> = Flatten<T & { [key in U]: V }>
 
 
-type AppendToObject<T extends {}, U extends PropertyKey, V> = 
-  {
-    [K in keyof T | U]: (T & Record<U, V>)[K]
-  }
+type AppendToObject<T extends {}, U extends PropertyKey, V> =
+    {
+        [K in keyof T | U]: (T & Record<U, V>)[K]
+    }
+
+type AppendToObject<T, U extends PropertyKey, V> = {
+    [K in keyof T | U]: K extends keyof T ? T[K] : V
+}
