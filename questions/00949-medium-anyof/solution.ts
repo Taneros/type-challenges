c1a7415efb968b08b3d1cc87fd7893046a37c9f6
Renat <fricentaurus@GMAIL.COM>
2024-06-12 19:29:48 +0400

    add solution anyof

--- questions/00949-medium-anyof/solution.ts
+++ questions/00949-medium-anyof/solution.ts
@@ -1,9 +1,14 @@
 /* _____________ Your Code Here _____________ */
 type Falsy = Record<string, never> | [] | 0 | '' | null | undefined | false
 
-type AnyOf<T extends unknown[]> = 
+type AnyOf<T extends unknown[]> =
   T extends [infer Head, ...infer Rest]
   ? Head extends Falsy
-    ? AnyOf<Rest>
-    : true
+  ? AnyOf<Rest>
+  : true
   : false
+
+
+type IsTruthy<T> = T extends Falsy ? false : true
+
+type AnyOf<T extends unknown[]> = IsTruthy<T[number]> extends false ? false : true
